5a72ef2e57559704812a1b09b76ea8c98e66c319
Dan Jaschob <djaschob@uw.edu>
2025-08-20 09:22:03 -0700

    Update to output version based on GIT release tag
    The gradle build uses environment variable 'LIMELIGHT_RELEASE_TAG'.
    The Github Action build sets 'LIMELIGHT_RELEASE_TAG' from the GIT
    release tag:
    
       env:
       	  LIMELIGHT_RELEASE_TAG: ${{ github.event.release.tag_name }}

--- src/main/java/org/yeastrc/limelight/xml/msfragger/constants/Constants.java
+++ src/main/java/org/yeastrc/limelight/xml/msfragger/constants/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
 
 	public static final String CONVERSION_PROGRAM_NAME = "msFragger2LimelightXML.jar";
 	public static final String CONVERSION_PROGRAM_URI = "https://github.com/yeastrc/limelight-import-msfragger-tsv";
-	public static final String CONVERSION_PROGRAM_VERSION = "1.0.0";
+	
+	//  Removed as now retrieved from file which is populated from environment variable which is populated from GIT release tag
+//	public static final String CONVERSION_PROGRAM_VERSION = "1.0.0";
 	
 }
